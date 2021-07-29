2b4438f174f48672be683b9a8cf91f4b8b72c12a
Qphuc <tqp1610@gmail.com>
2021-07-29 22:19:11 +0700

    reactive replace

--- src/components/Computed.vue
+++ src/components/Computed.vue
@@ -8,11 +8,15 @@
         <input type="text" placeholder="First Name" v-model="refFirstName">
         <input type="text" placeholder="Last Name" v-model="refLastName">
         <h2>Conposition Fullname is {{ refFullName }}</h2>
+
+        <input type="text" placeholder="First Name" v-model="reactiveFirstName">
+        <input type="text" placeholder="Last Name" v-model="reactiveLastName">
+        <h2>Reactive Fullname is {{ reactiveFullName }}</h2>
     </div>
 </template>
 
 <script>
-    import {ref, computed} from 'vue'
+    import {ref, computed, reactive, toRefs} from 'vue'
     export default {
         name: 'Computed',
         setup(props) {
@@ -23,10 +27,21 @@
                 return `${refFirstName.value} ${refLastName.value}`
             })
 
+            const state = reactive({
+                reactiveFirstName: '',
+                reactiveLastName: '',
+            })
+
+            const reactiveFullName = computed(function() {
+                return `${state.reactiveFirstName} ${state.reactiveLastName}`
+            })
+
             return {
                 refFirstName,
                 refLastName,
-                refFullName
+                refFullName,
+                ...toRefs(state),
+                reactiveFullName,
             }
         },
         data() {
