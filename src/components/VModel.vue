103915daab22c2cb4f7274e9078220d118cf1705
Qphuc <tqp1610@gmail.com>
2021-07-29 21:28:58 +0700

    v-model

---
+++ src/components/VModel.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <!-- Option API -->
+        <input type="text" placeholder="First Name" v-model="fName">
+        <input type="text" placeholder="Last     Name" v-model="lName">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'VModel',
+        data() {
+            return {
+                fName: '',
+                lName: '',
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
