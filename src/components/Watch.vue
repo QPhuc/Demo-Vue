b63fdea5f1b50908e85d1d5b2d1d0b7e7e43d637
Qphuc <tqp1610@gmail.com>
2021-07-30 21:41:30 +0700

    watch part 1

--- src/components/Watch.vue
+++ src/components/Watch.vue
@@ -1,12 +1,33 @@
 <template>
     <div>
         <input type="text" placeholder="Name" v-model="name">
+
+
+        <input type="text" placeholder="First Name" v-model="firstName">
+        <input type="text" placeholder="Last Name" v-model="lastName">
     </div>
 </template>
 
 <script>
+    import { ref, watch } from 'vue';
     export default {
         name: 'Watch',
+        setup(props) {
+            const firstName = ref('')
+            const lastName = ref('Phuc')
+
+            watch([firstName, lastName], (newValue, oldValue) => {
+                console.log('firstName Old value', oldValue[0]);
+                console.log('firstName New value', newValue[0]);
+                console.log('lastName Old value', oldValue[1]);
+                console.log('lastName New value', newValue[1]);
+            })
+
+            return {
+                firstName,
+                lastName
+            }
+        },
         data() {
             return {
                 name: '',
