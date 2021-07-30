d4eef601207d0dd87f50a6d4cbee52c1f2446839
Qphuc <tqp1610@gmail.com>
2021-07-30 22:18:11 +0700

    childA

---
+++ src/components/ChildA.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ChildA',
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/ProvideInject.vue
+++ src/components/ProvideInject.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
-
+        <h3>Parent component {{name}} </h3>
+        <ChildA />
     </div>
 </template>
 
 <script>
+    import ChildA from './ChildA.vue'
     export default {
+        component: { ChildA },
         name: 'ProvideInject',
+        data() {
+            return {
+                name: 'QPhuc'
+            }
+        },
+        provide() {
+            return {
+                userName: this.name
+            }
+        }
     }
 </script>
 
