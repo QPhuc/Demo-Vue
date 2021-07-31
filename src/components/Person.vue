df1ab1ebcb21ac07ee9811908d8bb286a4b258ce
Qphuc <tqp1610@gmail.com>
2021-07-31 19:54:17 +0700

    create Person

---
+++ src/components/Person.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <input type="text" placeholder="First Name" v-model="firstName">
+        <input type="text" placeholder="Last Name" v-model="lastName">
+    </div>
+</template>
+
+<script>
+    import { ref } from 'vue'
+    export default {
+        name: 'Person',
+        components: {
+
+        },
+        setup(props) {
+            const firstName = ref('')
+            const lastName = ref('')
+
+            return {
+                firstName,
+                lastName
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
